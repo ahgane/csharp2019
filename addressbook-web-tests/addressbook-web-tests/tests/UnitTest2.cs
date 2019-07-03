c8d5923d6513bfa801f257e2de4e2582f49cc1c5
emikheeva <emikheeva@nz.imshealth.com>
2019-07-04 10:57:58 +1200

    C и C++ cycles

---
+++ addressbook-web-tests/addressbook-web-tests/tests/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_tests.tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] s = new string[] {"test", "I", "Want", "to", "sleep" };
+            for (int i = 0; i < s.Length; i = i + 1)
+            {
+                System.Console.Out.Write(s[i] + "\n");
+            }
+        }
+    }
+}
